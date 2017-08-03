0bc23099e8235f0bc5d59b4cefdfb926d73ef382
Jean Gwennael <gwennael.jean@trsb.net>
2017-08-03 17:39:02 +0200

    test: fonctionnement de l'insertion + decommentation autre tests

--- test/index.php
+++ test/index.php
@@ -5,16 +5,54 @@ use Tourbillon\Dbal\ConnectionFactory;
 
 require '../vendor/autoload.php';
 
-$configurator = ConfiguratorFactory::createInstance(realpath(__DIR__.'/config/database.neon'));
+$configurator = ConfiguratorFactory::createInstance(realpath(__DIR__ . '/config/database.neon'));
 
 $connectionFactory = new ConnectionFactory($configurator);
 
 $connection = $connectionFactory->getConnection('default');
 
-$stmt = $connection->query("SELECT * FROM user WHERE created_at < :date", [
-    'date' => new \DateTime()
-]);
+// ---
+// UTILISATION DE BASE
+// ---
 
-while ($row = $stmt->fetch()) {
+$stmt = $connection->query("SELECT * FROM user WHERE created_at < :date");
+$connection->bindParam($stmt, 'date', new DateTime());
+$connection->execute($stmt);
+
+while ($row = $connection->fetch($stmt)) {
     var_dump($row);
 }
+
+echo "<hr/>";
+
+// ---
+// UTILISATION DU QUERY BUILDER
+// ---
+
+$queryBuilder = $connection->createQueryBuilder()
+        ->select('id', 'firstname')
+        ->from('user')
+        ->where('id = :id')
+        ->setParameter('id', 1);
+
+$stmt = $connection->query($queryBuilder->getQuery());
+$connection->bindParams($stmt, $queryBuilder->getParameters());
+$connection->execute($stmt);
+
+var_dump($connection->fetch($stmt));
+
+echo "<hr/>";
+
+// ---
+// INSERTION
+// ---
+
+$queryBuilder = $connection->createQueryBuilder()
+        ->insert('user')
+        ->set('firstname', 'Test')
+        ->set('lastname', 'Test')
+        ->set('email', 'test.test@test.test')
+;
+
+var_dump($queryBuilder->getQuery(), $queryBuilder->getParameters());
+
